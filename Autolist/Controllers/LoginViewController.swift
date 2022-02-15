f99015bbab6a54e2203f9122b7a51b4a859c1436
Kiryl Kakareka <charleequa@gmail.com>
2022-02-15 22:52:35 +0300

    Add UI elements in VC Remove Sign In Buttons (now only for login + passed)

--- Autolist/Controllers/LoginViewController.swift
+++ Autolist/Controllers/LoginViewController.swift
@@ -12,42 +12,54 @@ class LoginViewController: UIViewController {
     private var loginTextField = TextField(title: "Login")
     private var passwordTextField = TextField(title: "Password")
     private var signInButton = ActionButton(title: "Log in")
-    private var appleSIButton = SignInButton(type: .apple)
-    private var googleSIButton = SignInButton(type: .google)
-    
-    override func loadView() {
-        super.loadView()
-        view.backgroundColor = .systemBackground
-        passwordTextField.isSecureTextEntry = true
-    }
+    private var forgotPasswordLabel = UILabel()
+    private var stackView: UIStackView!
+    private var signUpLabel = SignUpLabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configVC()
         configStackView()
+        setViewsConstraints()
+    }
+    
+    private func configVC() {
+        view.backgroundColor = .systemBackground
+        
+        passwordTextField.isSecureTextEntry = true
+        
+        forgotPasswordLabel.text = "Forgot password?"
+        forgotPasswordLabel.textAlignment = .right
+        forgotPasswordLabel.font = UIFont(name: "Rubik-Regular", size: UIFont.labelFontSize)
+        forgotPasswordLabel.textColor = .secondaryLabel
+        
+        view.addSubview(signUpLabel)
     }
     
     private func configStackView() {
-        let insideSV = UIStackView(arrangedSubviews: [appleSIButton, googleSIButton])
-        insideSV.axis = .horizontal
-        insideSV.distribution = .fillEqually
-        insideSV.spacing = 16
-        insideSV.translatesAutoresizingMaskIntoConstraints = false
-        let stackView = UIStackView(arrangedSubviews: [loginTextField,
+        stackView = UIStackView(arrangedSubviews: [loginTextField,
                                                        passwordTextField,
                                                        signInButton,
-                                                       insideSV])
+                                                       forgotPasswordLabel])
         stackView.axis = .vertical
-        stackView.distribution = .fillEqually
+        stackView.distribution = .fill
         stackView.spacing = 16
         stackView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(stackView)
-        
+    }
+    
+    private func setViewsConstraints() {
         NSLayoutConstraint.activate([
+            signInButton.heightAnchor.constraint(equalToConstant: 50),
+            loginTextField.heightAnchor.constraint(equalToConstant: 44),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 44),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
             stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
+            signUpLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
+            signUpLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
+            signUpLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
         ])
-        
     }
 }
 
--- Autolist/Views/SignUpLabel.swift
+++ Autolist/Views/SignUpLabel.swift
@@ -9,22 +9,39 @@ import UIKit
 
 class SignUpLabel: UILabel {
     
+    init() {
+        super.init(frame: UIScreen.main.bounds);
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func setup() {
+        textAlignment = .center
         let fontSize: CGFloat = 14
         let textString: NSMutableAttributedString
-        if let font = UIFont(name: "Rubik-Regular", size: UIFont.smallSystemFontSize) {
-            textString = NSMutableAttributedString(string: "Don’t have an account? Sign up.",
-                                                   attributes: [.font: font,.foregroundColor: UIColor.secondaryLabel, .kern: 0.5])
+        let font: UIFont
+        if let customFont = UIFont(name: "Rubik-Regular", size: UIFont.smallSystemFontSize) {
+            font = customFont
         } else {
-            textString = NSMutableAttributedString(string: "Don’t have an account? Sign up.",
-                                                   attributes: [.font: UIFont.systemFont(ofSize: fontSize, weight: .light),.foregroundColor: UIColor.secondaryLabel, .kern: 0.5])
+            font = UIFont.systemFont(ofSize: fontSize, weight: .light)
         }
+        textString = NSMutableAttributedString(string: "Don’t have an account? Sign up.",
+                                               attributes: [
+                                                .font: font,
+                                                .foregroundColor: UIColor.secondaryLabel,
+                                                    .kern: 0.5
+                                               ])
         let attrbs: [NSAttributedString.Key: Any] = [
-            .foregroundColor: UIColor.label,
-            .font: UIFont.systemFont(ofSize: fontSize, weight: .semibold)]
+            .foregroundColor: UIColor.systemBlue,
+            .font: UIFont.systemFont(ofSize: 14, weight: .semibold)
+        ]
         textString.addAttributes(attrbs, range: NSRange(location: 23, length: 8))
         attributedText = textString
         isUserInteractionEnabled = true
+        translatesAutoresizingMaskIntoConstraints = false
 //        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showSignUp))
 //        self.addGestureRecognizer(tapGesture)
     }
