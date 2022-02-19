d2c6bf656399c21fea80acaada945de81b169b26
Kiryl Kakareka <charleequa@gmail.com>
2022-02-19 18:41:43 +0300

    Init cell GarageCarTableViewCell

---
+++ Autolist/Views/GarageCarTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GarageCarTableViewCell.swift
+//  Autolist
+//
+//  Created by Кирилл Какареко on 19.02.2022.
+//
+
+import UIKit
+
+class GarageCarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
